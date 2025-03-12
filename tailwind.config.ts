f91af89fb893dd16ab4bd919dd17ce9598d816e7
好名字 <826531489@qq.com>
2025-03-12 23:02:05 +0800

    feat: 隐藏全局的下拉条，美化界面

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        colors: {
+          'custom-blue': '#1677ff',
+          'custom-blue-opacity': '#1677ff0f', // 背景颜色
+        },
+        backgroundColor: {
+          'custom-blue-opacity': '#1677ff0f', // 添加带有透明度的背景颜色
+        },
+        borderColor: {
+          'custom-blue-opacity': '#1677ff34', // 添加带有透明度的边框颜色
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 } satisfies Config;
